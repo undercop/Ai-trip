1321107817fb8fcb330ed5213100892da71e545a
karthik_raj <raj.karthik5566@gmail.com>
2025-03-16 21:58:20 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h2> hiii im raj</h2>
+     <Button>babyyy</Button>
+    </>
+  )
+}
+
+export default App
