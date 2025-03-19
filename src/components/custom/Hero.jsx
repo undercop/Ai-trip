0bae8bee8f723577ad96e918082f049f15a8ae2d
karthik_raj <raj.karthik5566@gmail.com>
2025-03-19 22:15:04 +0530

    erroe

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import cheking from './cheking'
 function Hero() {
   return (
     <div className="flex items-center mx-56 gap-9">
+      <checking/>
       <h1
       className='font-extrabold text-[50px] text-center mt-16' 
      
---
+++ src/components/custom/cheking.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function cheking() {
+  return (
+    <div className=' p-10 m-30 bg-amber-700'>
+      hii i am debugger
+    </div>
+  )
+}
+
+export default cheking
