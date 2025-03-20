5225a5193ec335cd0638a23fd728fdeef950ed01
karthik_raj <raj.karthik5566@gmail.com>
2025-03-20 23:31:10 +0530

    phase 2 complete

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import cheking from './cheking'
+import cheking from './cheking.jsx'
+import { Button } from '../ui/button'
 function Hero() {
   return (
-    <div className="flex items-center mx-56 gap-9">
-      <checking/>
+    <div className="flex flex-col items-center mx-56 gap-9">
+      
       <h1
       className='font-extrabold text-[50px] text-center mt-16' 
      
@@ -15,6 +16,10 @@ function Hero() {
             <br/>
             :Personalised ltineraies at Your Fingertips
       </h1>
+      <p className='text-xl text-gray-500 text-center'>your personal trip planner and travel curator, creating custom itineraies tailored to your intrests and budget</p>
+      <Button>
+        Get Started, it's Free
+      </Button>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import CreateTrip from './create-trip'
+import CreateTrip from './create-trip/index.jsx'
 import Header from './components/custom/Header'
 
 const router = createBrowserRouter([
